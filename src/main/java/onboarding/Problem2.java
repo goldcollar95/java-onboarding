68247557d45eb15b6b6d09d8e7e5296d372a3af7
goldcollar95 <yjs7298@naver.com>
2022-11-23 21:19:40 +0900

    delete problem 1,2,3,4,5,6,7

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,25 +1,8 @@
 package onboarding;
 
-import java.util.Stack;
-
 public class Problem2 {
     public static String solution(String cryptogram) {
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : cryptogram.toCharArray()) {
-            if (!stack.isEmpty() && stack.peek() == c) {
-                stack.pop();
-            } else {
-                stack.push(c);
-            }
-        }
-
-        StringBuilder answer = new StringBuilder();
-
-        for (char c : stack) {
-            answer.append(c);
-        }
-
-        return answer.toString();
+        String answer = "answer";
+        return answer;
     }
 }
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,34 +6,6 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
-        while (money > 0) {
-            answer.add(money/50000);
-            money %= 50000;
-
-            answer.add(money/10000);
-            money %= 10000;
-
-            answer.add(money/5000);
-            money %= 5000;
-
-            answer.add(money/1000);
-            money %= 1000;
-
-            answer.add(money/500);
-            money %= 500;
-
-            answer.add(money/100);
-            money %= 100;
-
-            answer.add(money/50);
-            money %= 50;
-
-            answer.add(money/10);
-            money %= 10;
-
-            answer.add(money);
-            money = 0;
-        }
         return answer;
     }
 }
