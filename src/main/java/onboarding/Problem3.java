5197631a032ab497fa2d9441171f572f766eaa8b
goldcollar95 <yjs7298@naver.com>
2022-11-26 13:27:31 +0900

    refactor(Problem3): String->Char Data Type Change

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,16 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+    int answer = 0;
+    for(int index = 0; index <= number; index++){
+        String str = Integer.toString(index);
+        for(int toString = 0; toString < str.length(); toString++){
+            char chr = str.charAt((toString));
+            if(chr == '3' || chr == '6' || chr == '9'){
+                answer++;
+            }
+        }
+    }
         return answer;
     }
 }
