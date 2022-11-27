28a892955af41f64f48323dacc54b568beeabdb0
goldcollar95 <yjs7298@naver.com>
2022-11-27 23:58:26 +0900

    feat(Problem5): list -> List(add)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,25 +9,26 @@ public class Problem4 {
         String[] list = {"Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"};
 
 
-        Map<String,String> check = new HashMap<>();
+        Map<String, String> check = new HashMap<>();
 
-        for(int i = 0; i < list.length; i++){
-            check.put(Character.toString('A' + i), list[i]);
+        for (int index = 0; index < list.length; index++) {
+            check.put(Character.toString('A' + index), list[index]);
+
         }
 
-        String[] arrSplice = word.split("");
-        for(String c : arrSplice){
+        String[] arrSplice = word.split(" ");
+
+        for (String c : arrSplice) {
             String reverse = check.get(c.toUpperCase());
 
-            if(reverse == null){
+            if (reverse == null) {
                 reverse = " ";
             }
-            if(!(Character.isUpperCase(c.charAt(0)))){
+            if (!(Character.isUpperCase(c.charAt(0)))) {
                 reverse = reverse.toLowerCase();
             }
             answer += reverse;
         }
         return answer;
-
     }
 }
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,16 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
+
+        int[] list = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for (int i = 0; i < list.length; i++) {
+            if (i == 0) {
+                answer.add(money / list[i]);
+            } else {
+                answer.add(((money % list[i - 1]) / list[i]));
+            }
+
+        }
         return answer;
     }
 }
